c22c87fb1c6e26debf3379be5bbebf965f0b303b
bogosmith <bogosmith@yahoo.com>
2017-01-11 21:48:43 +0100

    Adding new files forgotten in the previous commit.

---
+++ dataman/src/ec/estat/bdcomp/data/TourismHotels.java
@@ -0,0 +1,35 @@
+package ec.estat.bdcomp.data;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Vector;
+
+import ec.estat.bdcomp.data.Indicator.Periodicity;
+import ec.estat.bdcomp.data.Indicator.Type;
+
+public class TourismHotels extends Indicator {
+	
+	public TourismHotels(boolean sa) {
+		this.p = Periodicity.MONTHLY;
+		this.t = Type.COUNT;
+		this.formatter = new SimpleDateFormat("yyyy-MM");
+		this.sa = sa;
+	}
+	
+	public String getFilename() {
+		return  ("tour_accom");	
+	}
+	
+	public Vector<String> getCountryAbbreviations() {
+		String[] s = new String[]{"AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "El", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "SP", "SE", "UK", "EA19", "EA", "EU28"};
+		return (new Vector<String>(Arrays.asList(s)));
+	}
+	public boolean equals(Indicator j) {		
+		if (!(super.equals(j))) { return false;};
+		if (! (j instanceof TourismHotels) || this.sa != j.isSa() || this.p != j.getPeriodicity() || this.t != j.getType()) {
+			return false;
+		} else {
+			return true;
+		}	
+	}
+}
